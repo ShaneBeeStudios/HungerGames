0346a2858c37d28ee2ec88e868bbbb86222282b3
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-17 04:00:40 -0700

    WandCmd - Changes - Added a name to the tool - Added some lore to the tool

--- src/me/minebuilders/hg/commands/WandCmd.java
+++ src/me/minebuilders/hg/commands/WandCmd.java
@@ -4,8 +4,13 @@ import me.minebuilders.hg.HG;
 import me.minebuilders.hg.PlayerSession;
 import me.minebuilders.hg.Util;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WandCmd extends BaseCmd {
 
@@ -21,10 +26,19 @@ public class WandCmd extends BaseCmd {
 			HG.plugin.playerses.remove(player.getUniqueId());
 			Util.msg(player, "Wand disabled!");
 		} else {
-			player.getInventory().addItem(new ItemStack(Material.BLAZE_ROD, 1));
+			ItemStack wand = new ItemStack(Material.BLAZE_ROD, 1);
+			ItemMeta meta = wand.getItemMeta();
+			meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&3HungerGames Wand"));
+			meta.setLore(new ArrayList<>(Arrays.asList(
+					ChatColor.translateAlternateColorCodes('&', "&7Left-Click to set position 1"),
+					ChatColor.translateAlternateColorCodes('&', "&7Right-Click to set position 2")
+			)));
+			wand.setItemMeta(meta);
+			player.getInventory().addItem(wand);
 			HG.plugin.playerses.put(player.getUniqueId(), new PlayerSession(null, null));
 			Util.msg(player, "Wand enabled!");
 		}
-        return true;
+		return true;
 	}
+
 }
