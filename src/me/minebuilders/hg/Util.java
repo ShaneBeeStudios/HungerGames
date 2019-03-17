dfdb85cdebe48e9310de0ee4c1ceb71669f2a1c0
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-16 23:37:16 -0700

    Util - changes - Code cleanup - Change messages

--- src/me/minebuilders/hg/Util.java
+++ src/me/minebuilders/hg/Util.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+import net.milkbowl.vault.chat.Chat;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
@@ -23,75 +24,86 @@ import org.bukkit.material.MaterialData;
 
 public class Util {
 
-	public static final BlockFace[] faces = new BlockFace[] {BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH};
-	
+	private static String prefix = "&7[&3HungerGames&7] ";
+
+	public static final BlockFace[] faces = new BlockFace[]{BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH};
+
 	private static final Logger log = Logger.getLogger("Minecraft");
 
-	public static void log(String s) { log.info("[HungerGames] " + s); }
-	
-	public static void warning(String s) { log.warning("[HungerGames] " + s); }
+	public static void log(String s) {
+		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + s));
+	}
+
+	public static void warning(String s) {
+		log.warning(prefix + s);
+	}
 
 	public static boolean hp(Player p, String s) {
-		if (p.hasPermission("hg." + s)) {
-			return true;
-		}
-		return false;
+		return p.hasPermission("hg." + s);
 	}
 
 	public static void msg(CommandSender sender, String s) {
-		sender.sendMessage(ChatColor.DARK_RED + "[" + ChatColor.GOLD + "HungerGames" +ChatColor.DARK_RED +"] " + ChatColor.YELLOW + ChatColor.translateAlternateColorCodes('&', s)); 
-	}
-	
-	public static void scm(CommandSender sender, String s) {
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s)); 
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
 	}
 
-	public static void broadcast(String s) { 
-		Bukkit.getServer().broadcastMessage(ChatColor.DARK_RED + "[" + ChatColor.GOLD + "HungerGames" +ChatColor.DARK_RED +"] " + ChatColor.YELLOW + ChatColor.translateAlternateColorCodes('&', s)); 
+	public static void scm(CommandSender sender, String s) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+	}
+
+	public static void broadcast(String s) {
+		Bukkit.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + s));
 	}
 
 	public static boolean isInt(String str) {
 		try {
 			Integer.parseInt(str);
-		} catch (NumberFormatException e) { return false; }
+		} catch (NumberFormatException e) {
+			return false;
+		}
 		return true;
 	}
 
 	public static BlockFace getSignFace(int face) {
-		switch(face) {
-		case 2: return BlockFace.WEST;
-		case 4: return BlockFace.SOUTH;
-		case 3: return BlockFace.EAST;
-		case 5: return BlockFace.NORTH;
-		default:
-			return BlockFace.WEST;
+		switch (face) {
+			case 2:
+				return BlockFace.WEST;
+			case 4:
+				return BlockFace.SOUTH;
+			case 3:
+				return BlockFace.EAST;
+			case 5:
+				return BlockFace.NORTH;
+			default:
+				return BlockFace.WEST;
 		}
 	}
 
 	public static void clearInv(Player p) {
 		p.getInventory().clear();
 		p.getEquipment().clear();
-		p.getInventory().setHelmet((ItemStack)null);
-		p.getInventory().setChestplate((ItemStack)null);
-		p.getInventory().setLeggings((ItemStack)null);
-		p.getInventory().setBoots((ItemStack)null);
+		p.getInventory().setHelmet(null);
+		p.getInventory().setChestplate(null);
+		p.getInventory().setLeggings(null);
+		p.getInventory().setBoots(null);
 		p.updateInventory();
 	}
 
 	public static List<String> convertUUIDListToStringList(List<UUID> uuid) {
 		List<String> winners = new ArrayList<String>();
-		for (UUID id: uuid) {
+		for (UUID id : uuid) {
 			winners.add(getOnlinePlayer(id).getName());
-		} return winners;
+		}
+		return winners;
 	}
-	
+
 	public static Player getOnlinePlayer(UUID uuid) {
 		Player player = Bukkit.getPlayer(uuid);
 		if (player != null) {
 			return player;
-		} return null;
+		}
+		return null;
 	}
-	
+
 	public static String translateStop(List<String> win) {
 		String bc = null;
 		int count = 0;
@@ -104,10 +116,10 @@ public class Util {
 		return bc;
 	}
 
-	public static void shootFirework(Location l) {
-		Firework fw = (Firework) l.getWorld().spawn(l, Firework.class);
+	static void shootFirework(Location l) {
+		Firework fw = l.getWorld().spawn(l, Firework.class);
 		FireworkMeta fm = fw.getFireworkMeta();
-		List<Color> c = new ArrayList<Color>();
+		List<Color> c = new ArrayList<>();
 		c.add(Color.ORANGE);
 		c.add(Color.RED);
 		FireworkEffect e = FireworkEffect.builder().flicker(true).withColor(c).withFade(c).with(Type.BALL_LARGE).trail(true).build();
@@ -115,17 +127,15 @@ public class Util {
 		fm.setPower(2);
 		fw.setFireworkMeta(fm);
 	}
-	
-	public static boolean isAttached(Block base, Block attached) {
+
+	static boolean isAttached(Block base, Block attached) {
 		MaterialData bs = attached.getState().getData();
 
 		if (!(bs instanceof Attachable)) return false;
 
 		Attachable at = (Attachable) bs;
-		
-		if (attached.getRelative(at.getAttachedFace()).equals(base))
-			return true;
-		
-		return false;
+
+		return attached.getRelative(at.getAttachedFace()).equals(base);
+
 	}
 }
