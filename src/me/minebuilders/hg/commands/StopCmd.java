ad11157a9fa66ad24ae40d98835417dbb0ddcdcf
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-18 17:15:23 -0700

    StopCommand - Changes - Added /hg stop all (stops all active games)

--- src/me/minebuilders/hg/commands/StopCmd.java
+++ src/me/minebuilders/hg/commands/StopCmd.java
@@ -1,9 +1,8 @@
 package me.minebuilders.hg.commands;
 
-import org.bukkit.ChatColor;
-
 import me.minebuilders.hg.Game;
 import me.minebuilders.hg.HG;
+import me.minebuilders.hg.Status;
 import me.minebuilders.hg.Util;
 
 public class StopCmd extends BaseCmd {
@@ -18,12 +17,22 @@ public class StopCmd extends BaseCmd {
 
 	@Override
 	public boolean run() {
+		if (args[1].equalsIgnoreCase("all")) {
+			for (Game game : HG.plugin.games) {
+				if (game.getStatus() == Status.RUNNING || game.getStatus() == Status.WAITING ||
+						game.getStatus() == Status.BEGINNING || game.getStatus() == Status.COUNTDOWN) {
+					game.stop();
+				}
+			}
+			Util.scm(sender, "&aStopped all games");
+			return true;
+		}
 		Game g = HG.manager.getGame(args[1]);
 		if (g != null) {
 			g.stop();
 			Util.scm(sender, "&6" + args[1] + "&c Has been stopped!");
 		} else {
-			sender.sendMessage(ChatColor.RED + "This game does not exist!");
+			Util.scm(sender, "&cThe game &b" + args[1] + "&c does not exist!");
 		}
 		return true;
 	}
