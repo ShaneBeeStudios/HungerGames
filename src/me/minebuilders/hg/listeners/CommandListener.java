b1a8a5c75bce500d930dc0e1755822b2b27ad767
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-25 16:51:52 -0700

    CommandListener - Cleanup - Small code cleanup

--- src/me/minebuilders/hg/listeners/CommandListener.java
+++ src/me/minebuilders/hg/listeners/CommandListener.java
@@ -16,20 +16,20 @@ import java.util.List;
 
 public class CommandListener implements CommandExecutor, TabCompleter {
 
-	private final HG p;
+	private final HG plugin;
 
 	public CommandListener(HG plugin) {
-		this.p = plugin;
+		this.plugin = plugin;
 	}
 
 	public boolean onCommand(CommandSender s, Command command, String label, String[] args) {
-		if (args.length == 0 || !p.cmds.containsKey(args[0])) {
+		if (args.length == 0 || !plugin.cmds.containsKey(args[0])) {
 			Util.scm(s, "&4*&c&m                         &7*( &3&lHungerGames &7)*&c&m                          &4*");
-			for (BaseCmd cmd : p.cmds.values().toArray(new BaseCmd[0])) {
+			for (BaseCmd cmd : plugin.cmds.values().toArray(new BaseCmd[0])) {
 				if (s.hasPermission("hg." + cmd.cmdName)) Util.scm(s, "  &7&l- " + cmd.sendHelpLine());
 			}
 			Util.scm(s, "&4*&c&m                                                                             &4*");
-		} else p.cmds.get(args[0]).processCmd(p, s, args);
+		} else plugin.cmds.get(args[0]).processCmd(plugin, s, args);
 		return true;
 	}
 
@@ -37,7 +37,7 @@ public class CommandListener implements CommandExecutor, TabCompleter {
 	public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
 		if (args.length == 1) {
 			ArrayList<String> matches = new ArrayList<>();
-			for (String name : HG.plugin.cmds.keySet()) {
+			for (String name : plugin.cmds.keySet()) {
 				if (StringUtil.startsWithIgnoreCase(name, args[0])) {
 					if (sender.hasPermission("hg." + name))
 						matches.add(name);
@@ -67,7 +67,7 @@ public class CommandListener implements CommandExecutor, TabCompleter {
 					(args[0].equalsIgnoreCase("toggle"))) {
 				ArrayList<String> matchesDelete = new ArrayList<>();
 				if (args.length == 2) {
-					for (Game name : HG.plugin.games) {
+					for (Game name : plugin.games) {
 						if (StringUtil.startsWithIgnoreCase(name.getName(), args[1])) {
 							matchesDelete.add(name.getName());
 						}
@@ -77,7 +77,7 @@ public class CommandListener implements CommandExecutor, TabCompleter {
 			} else if (args[0].equalsIgnoreCase("kit")) {
 				if (args.length == 2) {
 					ArrayList<String> matchesKit = new ArrayList<>();
-					for (String name : HG.plugin.kit.kititems.keySet()) {
+					for (String name : plugin.kit.kititems.keySet()) {
 						if (StringUtil.startsWithIgnoreCase(name, args[1])) {
 							matchesKit.add(name);
 						}
