69e7b7f22e660050ec151603251d29d38fe6cd9c
ShaneBee <shanebolenback@me.com>
2020-01-13 00:10:01 -0800

    ReloadCmd - changes - Allow from console - Ref #73

--- src/main/java/tk/shanebee/hg/commands/ReloadCmd.java
+++ src/main/java/tk/shanebee/hg/commands/ReloadCmd.java
@@ -7,7 +7,7 @@ import tk.shanebee.hg.util.Util;
 public class ReloadCmd extends BaseCmd {
 
 	public ReloadCmd() {
-		forcePlayer = true;
+		forcePlayer = false;
 		cmdName = "reload";
 		argLength = 1;
 		forceInRegion = false;
@@ -15,22 +15,22 @@ public class ReloadCmd extends BaseCmd {
 
 	@Override
 	public boolean run() {
-		Util.scm(player, lang.cmd_reload_attempt);
+		Util.scm(sender, lang.cmd_reload_attempt);
 		plugin.stopAll();
 		//plugin.getArenaConfig().saveCustomConfig();
 		plugin.getArenaConfig().reloadCustomConfig();
 		plugin.getArenaConfig().load();
-		Util.scm(player, lang.cmd_reload_reloaded_arena);
+		Util.scm(sender, lang.cmd_reload_reloaded_arena);
 		plugin.getKitManager().getKits().clear();
 		plugin.getItemStackManager().setKits();
-		Util.scm(player, lang.cmd_reload_reloaded_kit);
+		Util.scm(sender, lang.cmd_reload_reloaded_kit);
 		plugin.getItems().clear();
 		plugin.getRandomItems().load();
-		Util.scm(player, lang.cmd_reload_reloaded_items);
+		Util.scm(sender, lang.cmd_reload_reloaded_items);
 		new Config(plugin);
-		Util.scm(player, lang.cmd_reload_reloaded_config);
+		Util.scm(sender, lang.cmd_reload_reloaded_config);
 
-		Util.scm(player, lang.cmd_reload_reloaded_success);
+		Util.scm(sender, lang.cmd_reload_reloaded_success);
 		return true;
 	}
 
