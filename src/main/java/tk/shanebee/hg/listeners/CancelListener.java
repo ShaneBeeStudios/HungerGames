6d40535871a133ae7455b5f567d036164b4f9eba
ShaneBee <shanebolenback@me.com>
2020-03-25 20:51:03 -0700

    CancelListener - changes - Remove HG KIT canceller (handled in kit command) - Fixed an issue with the TP command (wrong player checking for data) - Updated formatting to spaces

--- src/main/java/tk/shanebee/hg/listeners/CancelListener.java
+++ src/main/java/tk/shanebee/hg/listeners/CancelListener.java
@@ -18,39 +18,30 @@ import java.util.UUID;
  */
 public class CancelListener implements Listener {
 
-	private PlayerManager playerManager;
+    private final PlayerManager playerManager;
 
-	public CancelListener(HG instance) {
-		this.playerManager = instance.getPlayerManager();
-	}
+    public CancelListener(HG instance) {
+        this.playerManager = instance.getPlayerManager();
+    }
 
-	@EventHandler(priority=EventPriority.LOWEST)
-	private void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		Player player = event.getPlayer();
-		if (player.hasPermission("hg.command.bypass")) return;
+    @EventHandler(priority = EventPriority.LOWEST)
+    private void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasPermission("hg.command.bypass")) return;
         UUID uuid = player.getUniqueId();
-		String[] st = event.getMessage().split(" ");
-		if (playerManager.hasData(uuid) && !st[0].equalsIgnoreCase("/login")) {
-			if (st[0].equalsIgnoreCase("/hg")) {
-				if (st.length >= 2 && st[1].equalsIgnoreCase("kit") && playerManager.getData(uuid).getGame().getStatus() == Status.RUNNING) {
-					event.setMessage("/");
-					event.setCancelled(true);
-					Util.scm(player, HG.getPlugin().getLang().cmd_handler_nokit);
-				}
-				return;
-			}
-			event.setMessage("/");
-			event.setCancelled(true);
-			Util.scm(player, HG.getPlugin().getLang().cmd_handler_nocmd);
-		} else if ("/tp".equalsIgnoreCase(st[0]) && st.length >= 2) {
-			Player p = Bukkit.getServer().getPlayer(st[1]);
-			if (p != null) {
-				if (playerManager.hasPlayerData(uuid)) {
-					Util.scm(player, HG.getPlugin().getLang().cmd_handler_playing);
-					event.setMessage("/");
-					event.setCancelled(true);
-				}
-			}
-		} 
-	}
+        String[] st = event.getMessage().split(" ");
+        if (playerManager.hasData(uuid) && !st[0].equalsIgnoreCase("/login")) {
+            event.setMessage("/");
+            event.setCancelled(true);
+            Util.scm(player, HG.getPlugin().getLang().cmd_handler_nocmd);
+        } else if ("/tp".equalsIgnoreCase(st[0]) && st.length >= 2) {
+            Player p = Bukkit.getServer().getPlayer(st[1]);
+            if (p != null && playerManager.hasPlayerData(p.getUniqueId())) {
+                Util.scm(player, HG.getPlugin().getLang().cmd_handler_playing);
+                event.setMessage("/");
+                event.setCancelled(true);
+            }
+        }
+    }
+
 }
