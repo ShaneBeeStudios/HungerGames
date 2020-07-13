d75b762c5eb97e1b2ab264b08adb51d3975c8a07
Shane Bee <shanebolenback@me.com>
2020-07-13 04:13:36 -0700

    Config - changes - Revamp config for better loading - Update config updater to be automatic when new config settings added - Fix an issue where modifying kits weren't taking effect in game - Ref #34

--- src/main/java/tk/shanebee/hg/data/Config.java
+++ src/main/java/tk/shanebee/hg/data/Config.java
@@ -1,11 +1,15 @@
 package tk.shanebee.hg.data;
 
 import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import tk.shanebee.hg.HG;
 import tk.shanebee.hg.util.Util;
 import tk.shanebee.hg.util.Vault;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -13,119 +17,128 @@ import java.util.List;
  */
 public class Config {
 
-	//Basic settings
-	public static boolean economy = true;
-	public static boolean spawnmobs;
-	public static int spawnmobsinterval;
-	public static boolean bossbar;
-	public static int trackingstickuses;
-	public static int playersfortrackingstick;
-	public static int maxchestcontent;
-	public static int minchestcontent;
-	public static int maxbonuscontent;
-	public static int minbonuscontent;
-	public static int maxTeam;
-	public static boolean teleportEnd;
-	public static int teleportEndTime;
+    //Basic settings
+    public static boolean economy = true;
+    public static boolean spawnmobs;
+    public static int spawnmobsinterval;
+    public static boolean bossbar;
+    public static int trackingstickuses;
+    public static int playersfortrackingstick;
+    public static int maxchestcontent;
+    public static int minchestcontent;
+    public static int maxbonuscontent;
+    public static int minbonuscontent;
+    public static int maxTeam;
+    public static boolean teleportEnd;
+    public static int teleportEndTime;
 
-	//Reward info
-	public static boolean giveReward;
-	public static int cash;
-	public static List<String> rewardCommands;
-	public static List<String> rewardMessages;
+    //Reward info
+    public static boolean giveReward;
+    public static int cash;
+    public static List<String> rewardCommands;
+    public static List<String> rewardMessages;
 
-	//Rollback config info
-	public static boolean breakblocks;
-	public static int blocks_per_second;
-	public static boolean protectCooldown;
-	public static boolean fixleaves;
-	public static boolean preventtrample;
-	public static List<String> blocks;
+    //Rollback config info
+    public static boolean breakblocks;
+    public static int blocks_per_second;
+    public static boolean protectCooldown;
+    public static boolean fixleaves;
+    public static boolean preventtrample;
+    public static List<String> blocks;
 
-	//Random chest
-	public static boolean randomChest;
-	public static int randomChestInterval;
-	public static int randomChestMaxContent;
+    //Random chest
+    public static boolean randomChest;
+    public static int randomChestInterval;
+    public static int randomChestMaxContent;
 
-	//World border
-	public static boolean borderEnabled;
-	public static boolean borderOnStart;
-	public static boolean centerSpawn;
-	public static int borderCountdownStart;
-	public static int borderCountdownEnd;
-	public static int borderFinalSize;
+    //World border
+    public static boolean borderEnabled;
+    public static boolean borderOnStart;
+    public static boolean centerSpawn;
+    public static int borderCountdownStart;
+    public static int borderCountdownEnd;
+    public static int borderFinalSize;
 
-	//Spectate
-	public static boolean spectateEnabled;
-	public static boolean spectateOnDeath;
-	public static boolean spectateHide;
-	public static boolean spectateFly;
-	public static boolean spectateChat;
+    //Spectate
+    public static boolean spectateEnabled;
+    public static boolean spectateOnDeath;
+    public static boolean spectateHide;
+    public static boolean spectateFly;
+    public static boolean spectateChat;
 
-	//mcMMO
-	public static boolean mcmmoUseSkills;
-	public static boolean mcmmoGainExp;
+    //mcMMO
+    public static boolean mcmmoUseSkills;
+    public static boolean mcmmoGainExp;
 
-	private HG plugin;
+    private final HG plugin;
+    private File configFile;
+    private FileConfiguration config;
 
-	public Config(HG plugin) {
-		this.plugin = plugin;
-		if (!new File(plugin.getDataFolder(), "config.yml").exists()) {
-			Util.log("Config not found. Generating default config!");
-			plugin.saveDefaultConfig();
-		}
-		Configuration config = plugin.getConfig().getRoot();
-		assert config != null;
-		config.options().copyDefaults(true);
-		plugin.reloadConfig();
-		config = plugin.getConfig();
-		updateConfig(config);
-		Util.log("Config loaded!");
+    public Config(HG plugin) {
+        this.plugin = plugin;
+        loadConfigFile();
+    }
 
-		spawnmobs = config.getBoolean("settings.spawn-mobs");
-		spawnmobsinterval = config.getInt("settings.spawn-mobs-interval") * 20;
-		bossbar = config.getBoolean("settings.bossbar-countdown");
-		trackingstickuses = config.getInt("settings.trackingstick-uses");
-		playersfortrackingstick = config.getInt("settings.players-for-trackingstick");
-		maxchestcontent = config.getInt("settings.max-chestcontent");
-		minchestcontent = config.getInt("settings.min-chestcontent");
-		maxbonuscontent = config.getInt("settings.max-bonus-chestcontent");
-		minbonuscontent = config.getInt("settings.min-bonus-chestcontent");
-		maxTeam = config.getInt("settings.max-team-size");
-		giveReward = config.getBoolean("reward.enabled");
-		cash = config.getInt("reward.cash");
-		rewardCommands = config.getStringList("reward.commands");
-		rewardMessages = config.getStringList("reward.messages");
-		maxTeam = config.getInt("settings.max-team-size");
-		giveReward = config.getBoolean("reward.enabled");
-		cash = config.getInt("reward.cash");
-		breakblocks = config.getBoolean("rollback.allow-block-break");
-		blocks_per_second = config.getInt("rollback.blocks-per-second");
-		protectCooldown = config.getBoolean("rollback.protect-during-cooldown");
-		fixleaves = config.getBoolean("rollback.fix-leaves");
-		preventtrample = config.getBoolean("rollback.prevent-trampling");
-		blocks = config.getStringList("rollback.editable-blocks");
-		randomChest = config.getBoolean("random-chest.enabled");
-		randomChestInterval = config.getInt("random-chest.interval") * 20;
-		randomChestMaxContent = config.getInt("random-chest.max-chestcontent");
-		teleportEnd = config.getBoolean("settings.teleport-at-end");
-		teleportEndTime = config.getInt("settings.teleport-at-end-time");
+    private void loadConfigFile() {
+        if (configFile == null) {
+            configFile = new File(plugin.getDataFolder(), "config.yml");
+        }
+        if (!configFile.exists()) {
+            plugin.saveResource("config.yml", false);
+            Util.log("&7New config.yml created");
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+        matchConfig(config, configFile);
+        loadConfig();
+        Util.log("&7config.yml loaded");
+    }
 
-		borderEnabled = config.getBoolean("world-border.enabled");
-		borderOnStart = config.getBoolean("world-border.initiate-on-start");
-		centerSpawn = config.getBoolean("world-border.center-on-first-spawn");
-		borderCountdownStart = config.getInt("world-border.countdown-start");
-		borderCountdownEnd = config.getInt("world-border.countdown-end");
-		borderFinalSize = config.getInt("world-border.final-border-size");
 
-		spectateEnabled = config.getBoolean("spectate.enabled");
-		spectateOnDeath = config.getBoolean("spectate.death-to-spectate");
-		spectateHide = config.getBoolean("spectate.hide-spectators");
-		spectateFly = config.getBoolean("spectate.fly");
-		spectateChat = config.getBoolean("spectate.chat");
+    private void loadConfig() {
+        spawnmobs = config.getBoolean("settings.spawn-mobs");
+        spawnmobsinterval = config.getInt("settings.spawn-mobs-interval") * 20;
+        bossbar = config.getBoolean("settings.bossbar-countdown");
+        trackingstickuses = config.getInt("settings.trackingstick-uses");
+        playersfortrackingstick = config.getInt("settings.players-for-trackingstick");
+        maxchestcontent = config.getInt("settings.max-chestcontent");
+        minchestcontent = config.getInt("settings.min-chestcontent");
+        maxbonuscontent = config.getInt("settings.max-bonus-chestcontent");
+        minbonuscontent = config.getInt("settings.min-bonus-chestcontent");
+        maxTeam = config.getInt("settings.max-team-size");
+        giveReward = config.getBoolean("reward.enabled");
+        cash = config.getInt("reward.cash");
+        rewardCommands = config.getStringList("reward.commands");
+        rewardMessages = config.getStringList("reward.messages");
+        maxTeam = config.getInt("settings.max-team-size");
+        giveReward = config.getBoolean("reward.enabled");
+        cash = config.getInt("reward.cash");
+        breakblocks = config.getBoolean("rollback.allow-block-break");
+        blocks_per_second = config.getInt("rollback.blocks-per-second");
+        protectCooldown = config.getBoolean("rollback.protect-during-cooldown");
+        fixleaves = config.getBoolean("rollback.fix-leaves");
+        preventtrample = config.getBoolean("rollback.prevent-trampling");
+        blocks = config.getStringList("rollback.editable-blocks");
+        randomChest = config.getBoolean("random-chest.enabled");
+        randomChestInterval = config.getInt("random-chest.interval") * 20;
+        randomChestMaxContent = config.getInt("random-chest.max-chestcontent");
+        teleportEnd = config.getBoolean("settings.teleport-at-end");
+        teleportEndTime = config.getInt("settings.teleport-at-end-time");
 
-		mcmmoUseSkills = config.getBoolean("mcmmo.use-skills");
-		mcmmoGainExp = config.getBoolean("mcmmo.gain-experience");
+        borderEnabled = config.getBoolean("world-border.enabled");
+        borderOnStart = config.getBoolean("world-border.initiate-on-start");
+        centerSpawn = config.getBoolean("world-border.center-on-first-spawn");
+        borderCountdownStart = config.getInt("world-border.countdown-start");
+        borderCountdownEnd = config.getInt("world-border.countdown-end");
+        borderFinalSize = config.getInt("world-border.final-border-size");
+
+        spectateEnabled = config.getBoolean("spectate.enabled");
+        spectateOnDeath = config.getBoolean("spectate.death-to-spectate");
+        spectateHide = config.getBoolean("spectate.hide-spectators");
+        spectateFly = config.getBoolean("spectate.fly");
+        spectateChat = config.getBoolean("spectate.chat");
+
+        mcmmoUseSkills = config.getBoolean("mcmmo.use-skills");
+        mcmmoGainExp = config.getBoolean("mcmmo.gain-experience");
 
         try {
             Vault.setupEconomy();
@@ -142,24 +155,38 @@ public class Config {
             giveReward = false;
             economy = false;
         }
-	}
+    }
 
-	private void updateConfig(Configuration config) {
-		if (!config.isSet("spectate.enabled")) {
-			config.set("spectate.enabled", false);
-			config.set("spectate.death-to-spectate", true);
-			config.set("spectate.hide-spectators", true);
-			config.set("spectate.fly", true);
-		}
-		if (!config.isSet("rollback.blocks-per-second")) {
-			config.set("rollback.blocks-per-second", 500);
-		}
-		if (!config.isSet("spectate.chat")) {
-			config.set("spectate.chat", false);
-			config.set("mcmmo.use-skills", false);
-			config.set("mcmmo.gain-experience", false);
-		}
-		plugin.saveConfig();
-	}
+    // Used to update config
+    @SuppressWarnings("ConstantConditions")
+    private void matchConfig(FileConfiguration config, File file) {
+        try {
+            boolean hasUpdated = false;
+            InputStream test = plugin.getResource(file.getName());
+            assert test != null;
+            InputStreamReader is = new InputStreamReader(test);
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(is);
+            for (String key : defConfig.getConfigurationSection("").getKeys(true)) {
+                if (!config.contains(key) && !key.contains("kits")) {
+                    config.set(key, defConfig.get(key));
+                    hasUpdated = true;
+                }
+            }
+            for (String key : config.getConfigurationSection("").getKeys(true)) {
+                if (!defConfig.contains(key) && !key.contains("kits.") && !key.contains("globalexit")) {
+                    config.set(key, null);
+                    hasUpdated = true;
+                }
+            }
+            if (hasUpdated)
+                config.save(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Configuration getConfig() {
+        return config;
+    }
 
 }
