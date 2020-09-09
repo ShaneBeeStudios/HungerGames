302cd1e4eb9cf7d744bb302d2d049df4f9922a98
Shane Bee <shanebolenback@me.com>
2020-09-09 14:36:25 -0700

    FreeRoamTask - cleanup - Cleanup some messages - Only send roam time if > 0

--- src/main/java/tk/shanebee/hg/tasks/FreeRoamTask.java
+++ src/main/java/tk/shanebee/hg/tasks/FreeRoamTask.java
@@ -1,41 +1,53 @@
 package tk.shanebee.hg.tasks;
 
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
-import tk.shanebee.hg.game.Game;
 import tk.shanebee.hg.HG;
+import tk.shanebee.hg.data.Language;
+import tk.shanebee.hg.game.Game;
 import tk.shanebee.hg.util.Util;
+
+import java.util.UUID;
 
 public class FreeRoamTask implements Runnable {
 
-	private Game game;
-	private int id;
+    private final Game game;
+    private final int id;
+    private final int roamTime;
 
-	public FreeRoamTask(Game g) {
-		this.game = g;
-		for (UUID u : g.getGamePlayerData().getPlayers()) {
-			Player p = Bukkit.getPlayer(u);
-			if (p != null) {
-				Util.scm(p, HG.getPlugin().getLang().roam_game_started);
-				Util.scm(p, HG.getPlugin().getLang().roam_time.replace("<roam>", String.valueOf(g.getRoamTime())));
-				p.setHealth(20);
-				p.setFoodLevel(20);
-				g.getGamePlayerData().unFreeze(p);
-			}
-		}
-		this.id = Bukkit.getScheduler().scheduleSyncDelayedTask(HG.getPlugin(), this, g.getRoamTime() * 20L);
-	}
+    public FreeRoamTask(Game game) {
+        this.game = game;
+        this.roamTime = game.getRoamTime();
 
-	@Override
-	public void run() {
-		game.getGamePlayerData().msgAll(HG.getPlugin().getLang().roam_finished);
-		game.startGame();
-	}
+        Language lang = HG.getPlugin().getLang();
+        String gameStarted = lang.roam_game_started;
+        String roamTimeString = lang.roam_time.replace("<roam>", "" + roamTime);
 
-	public void stop() {
-		Bukkit.getScheduler().cancelTask(id);
-	}
+        for (UUID u : game.getGamePlayerData().getPlayers()) {
+            Player player = Bukkit.getPlayer(u);
+            if (player != null) {
+                Util.scm(player, gameStarted);
+                if (roamTime > 0) {
+                    Util.scm(player, roamTimeString);
+                }
+                player.setHealth(20);
+                player.setFoodLevel(20);
+                game.getGamePlayerData().unFreeze(player);
+            }
+        }
+        this.id = Bukkit.getScheduler().scheduleSyncDelayedTask(HG.getPlugin(), this, roamTime * 20L);
+    }
+
+    @Override
+    public void run() {
+        if (roamTime > 0) {
+            game.getGamePlayerData().msgAll(HG.getPlugin().getLang().roam_finished);
+        }
+        game.startGame();
+    }
+
+    public void stop() {
+        Bukkit.getScheduler().cancelTask(id);
+    }
+
 }
