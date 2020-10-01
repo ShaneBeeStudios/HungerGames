369b162b02f5328c337262e88d725424d8a486df
Shane Bee <shanebolenback@me.com>
2020-10-01 01:47:23 -0700

    ItemStackManager - cleanup/fix some issues with potions - Remove old deprecated potion code - Cleanup some other code

--- src/main/java/tk/shanebee/hg/managers/ItemStackManager.java
+++ src/main/java/tk/shanebee/hg/managers/ItemStackManager.java
@@ -1,6 +1,9 @@
 package tk.shanebee.hg.managers;
 
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
@@ -10,13 +13,13 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.Nullable;
 import tk.shanebee.hg.HG;
-import tk.shanebee.hg.util.PotionEffectUtils;
-import tk.shanebee.hg.util.Util;
 import tk.shanebee.hg.data.KitEntry;
 import tk.shanebee.hg.util.NBTApi;
+import tk.shanebee.hg.util.PotionEffectUtils;
+import tk.shanebee.hg.util.Util;
 
-import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -120,6 +123,8 @@ public class ItemStackManager {
         if (item == null) return null;
 
         for (String s : split) {
+            ItemMeta itemMeta = item.getItemMeta();
+            assert itemMeta != null;
             if (s.startsWith("enchant:")) {
                 s = s.replace("enchant:", "").toUpperCase();
                 String[] d = s.split(":");
@@ -133,156 +138,91 @@ public class ItemStackManager {
                     }
                 }
             } else if (s.startsWith("color:")) {
-                if (item.getItemMeta() instanceof LeatherArmorMeta) {
-                    LeatherArmorMeta meta = ((LeatherArmorMeta) item.getItemMeta());
-                    meta.setColor(getColor(s));
-                    item.setItemMeta(meta);
-                } else if (item.getItemMeta() instanceof PotionMeta) {
-                    PotionMeta meta = ((PotionMeta) item.getItemMeta());
-                    meta.setColor(getColor(s));
-                    item.setItemMeta(meta);
+                if (itemMeta instanceof LeatherArmorMeta) {
+                    ((LeatherArmorMeta) itemMeta).setColor(getColor(s));
+                } else if (itemMeta instanceof PotionMeta) {
+                    ((PotionMeta) itemMeta).setColor(getColor(s));
                 }
             } else if (s.startsWith("name:")) {
                 s = s.replace("name:", "").replace("_", " ");
-                s = ChatColor.translateAlternateColorCodes('&', s);
-                ItemMeta im = item.getItemMeta();
-                assert im != null;
-                im.setDisplayName(s);
-                item.setItemMeta(im);
+                s = Util.getColString(s);
+                itemMeta.setDisplayName(s);
             } else if (s.startsWith("lore:")) {
                 s = s.replace("lore:", "").replace("_", " ");
-                s = ChatColor.translateAlternateColorCodes('&', s);
-                ItemMeta meta = item.getItemMeta();
+                s = Util.getColString(s);
                 ArrayList<String> lore = new ArrayList<>(Arrays.asList(s.split(":")));
-                assert meta != null;
-                meta.setLore(lore);
-                item.setItemMeta(meta);
-            } else if (s.startsWith("potion:")) {
-                if (item.getItemMeta() instanceof PotionMeta) {
-                    s = s.replace("potion:", "");
-                    PotionMeta potionMeta = ((PotionMeta) item.getItemMeta());
-                    String[] effects = s.split(";");
-                    for (String effect : effects) {
-                        if (verifyPotionEffects(effect, false)) {
-                            String[] data = effect.split(":");
-                            PotionEffectType potType = PotionEffectUtils.get(data[0]);
-                            int duration = Integer.parseInt(data[1]);
-                            int amplifier = Integer.parseInt(data[2]);
-                            assert potionMeta != null;
-                            assert potType != null;
-                            PotionEffect potionEffect = new PotionEffect(potType, duration, amplifier);
-                            potionMeta.addCustomEffect(potionEffect, true);
-                        }
-                        item.setItemMeta(potionMeta);
+                itemMeta.setLore(lore);
+            } else if (s.startsWith("potion:") && itemMeta instanceof PotionMeta) {
+                s = s.replace("potion:", "");
+                String[] effects = s.split(";");
+                for (String effect : effects) {
+                    if (verifyPotionEffects(effect)) {
+                        String[] data = effect.split(":");
+                        PotionEffectType potType = PotionEffectUtils.get(data[0]);
+                        int duration = Integer.parseInt(data[1]);
+                        int amplifier = Integer.parseInt(data[2]);
+                        assert potType != null;
+                        PotionEffect potionEffect = new PotionEffect(potType, duration, amplifier);
+                        ((PotionMeta) itemMeta).addCustomEffect(potionEffect, true);
                     }
                 }
-
             } else if (s.startsWith("data:")) {
                 s = s.replace("data:", "").replace("~", " ");
                 if (nbtApi != null)
                     item = nbtApi.getItemWithNBT(item, s);
-            } else if (s.startsWith("ownerName:")) {
+            } else if (s.startsWith("ownerName:") && itemMeta instanceof SkullMeta) {
                 s = s.replace("ownerName:", "");
-                if (item.getItemMeta() instanceof SkullMeta) {
-                    ItemMeta meta = item.getItemMeta();
-                    assert meta != null;
-                    ((SkullMeta) meta).setOwningPlayer(Bukkit.getOfflinePlayer(s));
-                    item.setItemMeta(meta);
-                }
+                ((SkullMeta) itemMeta).setOwningPlayer(Bukkit.getOfflinePlayer(s));
             }
+            item.setItemMeta(itemMeta);
         }
         return item;
     }
 
     private ItemStack itemStringToStack(String item, int amount) {
-        String oldPotion = item.toUpperCase();
         String[] itemArr = item.split(":");
-        if (oldPotion.startsWith("POTION:") || oldPotion.startsWith("SPLASH_POTION:") || oldPotion.startsWith("LINGERING_POTION:")) {
-            Util.warning("Using old potion item format: &b" + oldPotion);
-            Util.warning("  - This format is deprecated and will be removed in the future, please use new format.");
-            return getPotion(item, amount);
-        }
-        Material mat = verifyMaterial(itemArr[0].toUpperCase());
-        if (mat == null) {
-            return null;
-        }
-        return new ItemStack(mat, amount);
-    }
-
-    private Material verifyMaterial(String material) {
-        Material mat;
+        String materialString = itemArr[0].toUpperCase();
+        Material material;
         try {
-            mat = Material.valueOf(material);
+            material = Material.valueOf(materialString);
         } catch (IllegalArgumentException ex) {
-            Util.warning("Invalid Material: &7" + material);
+            Util.warning("Invalid Material: &7" + materialString);
             return null;
         }
-        return mat;
-    }
-
-    // Get a potion item stack from a string
-    // DEPRECATED - will remove in future
-    private ItemStack getPotion(String item, int amount) {
-        String[] effects = item.split(";");
-        String potionType = item.split(":")[0];
-        ItemStack potion = new ItemStack(Material.valueOf(potionType.toUpperCase()), amount);
-        PotionMeta potionMeta = ((PotionMeta) potion.getItemMeta());
-
-        for (String effect : effects) {
-            if (!verifyPotionEffects(effect, true)) {
-                return null;
-            }
-
-            String[] data = effect.split(":");
-            int i = (data[0].contains("potion") || data[0].contains("POTION")) ? 1 : 0;
-            PotionEffectType potType = PotionEffectUtils.get(data[i]);
-            int duration = Integer.parseInt(data[1 + i]);
-            int amplifier = Integer.parseInt(data[2 + i]);
-            assert potionMeta != null;
-            assert potType != null;
-            potionMeta.addCustomEffect(new PotionEffect(potType, duration, amplifier), true);
-        }
-        potion.setItemMeta(potionMeta);
-        return potion;
+        return new ItemStack(material, amount);
     }
 
     // Verify if the potion effects are valid (including parameters)
-    private boolean verifyPotionEffects(String data, boolean potionItem) {
-        String pot = potionItem ? "POTION:" : "potion:";
+    private boolean verifyPotionEffects(String data) {
         String[] potionData = data.split(":");
         if (potionData.length == 3 || potionData.length == 4) {
             int i = potionData.length == 3 ? 0 : 1;
             if (PotionEffectUtils.get(potionData[i]) == null) {
-                Util.warning("Potion effect type not found: &c" + potionData[i].toUpperCase());
-                Util.log("  - Check your configs");
-                Util.log("  - Proper example:");
-                Util.log("      &b" + pot + ":POTION_TYPE:DURATION_IN_TICKS:AMPLIFIER");
-                Util.log("      &b" + pot + ":HEAL:200:1");
+                Util.warning("Potion effect type not found: &c" + potionData[i].toUpperCase() + " &ein: &b" + data);
+                potionWarning();
                 return false;
             } else if (!Util.isInt(potionData[i + 1])) {
-                Util.warning("Potion duration incorrect format: &c" + potionData[i + 1]);
-                Util.log("  - Check your configs");
-                Util.log("  - Proper example:");
-                Util.log("      &b" + pot + ":POTION_TYPE:DURATION_IN_TICKS:AMPLIFIER");
-                Util.log("      &b" + pot + ":HEAL:200:1");
+                Util.warning("Potion duration incorrect format: &c" + potionData[i + 1] + " &ein: &b" + data);
+                potionWarning();
                 return false;
             } else if (!Util.isInt(potionData[i + 2])) {
-                Util.warning("Potion amplifier incorrect format: &c" + potionData[i + 2]);
-                Util.log("  - Check your configs");
-                Util.log("  - Proper example:");
-                Util.log("      &b" + pot + ":POTION_TYPE:DURATION_IN_TICKS:AMPLIFIER");
-                Util.log("      &b" + pot + ":HEAL:200:1");
+                Util.warning("Potion amplifier incorrect format: &c" + potionData[i + 2] + " &ein: &b" + data);
+                potionWarning();
                 return false;
             }
         } else {
             Util.warning("Improper setup of potion: &c" + data);
-            Util.log("  - Check your configs for missing arguments");
-            Util.log("  - Proper example:");
-            Util.log("      &b" + pot + ":POTION_TYPE:DURATION_IN_TICKS:LEVEL");
-            Util.log("      &b" + pot + ":HEAL:200:1");
+            potionWarning();
             return false;
         }
         return true;
+    }
+
+    private void potionWarning() {
+        Util.warning("&r  - Check your configs");
+        Util.warning("&r  - Proper example:");
+        Util.warning("      &bpotion:POTION_TYPE:DURATION_IN_TICKS:LEVEL");
+        Util.warning("      &bpotion:HEAL:200:1");
     }
 
     private Color getColor(String colorString) {
