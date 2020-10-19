d13695d1ee07b9af12fb7eaa6cd95ef1da06cebd
Shane Bee <shanebolenback@me.com>
2020-10-18 23:54:54 -0700

    SpawnerTask - cleanup

--- src/main/java/tk/shanebee/hg/tasks/SpawnerTask.java
+++ src/main/java/tk/shanebee/hg/tasks/SpawnerTask.java
@@ -5,134 +5,94 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import tk.shanebee.hg.game.Game;
-import tk.shanebee.hg.HG;
 import tk.shanebee.hg.data.MobEntry;
+import tk.shanebee.hg.game.Game;
 import tk.shanebee.hg.managers.MobManager;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
 public class SpawnerTask implements Runnable {
 
-	private final Game game;
-	private final int id;
-	private final Random rg = new Random();
-	private final MobManager mobManager;
+    private final Game game;
+    private final int id;
+    private final Random random = new Random();
+    private final World world;
+    private final List<MobEntry> dayMobs;
+    private final List<MobEntry> nightMobs;
 
-	public SpawnerTask(Game game, int i) {
-		this.game = game;
-		this.id = Bukkit.getScheduler().scheduleSyncRepeatingTask(HG.getPlugin(), this, i, i);
-		this.mobManager = game.getMobManager();
-	}
+    public SpawnerTask(Game game, int i) {
+        this.game = game;
+        this.id = Bukkit.getScheduler().scheduleSyncRepeatingTask(game.getGameArenaData().getPlugin(), this, i, i);
+        this.world = game.getGameArenaData().getBound().getWorld();
+        MobManager mobManager = game.getMobManager();
+        this.dayMobs = mobManager.getDayMobs();
+        this.nightMobs = mobManager.getNightMobs();
+    }
 
-	@Override
-	public void run() {
-		for (UUID u : game.getGamePlayerData().getPlayers()) {
-			Player p = Bukkit.getPlayer(u);
-			if (p != null) {
-				Location location = p.getLocation();
-				World world = location.getWorld();
-				int x = location.getBlockX();
-				int z = location.getBlockZ();
-				int y = location.getBlockY();
+    @Override
+    public void run() {
+        for (UUID u : game.getGamePlayerData().getPlayers()) {
+            Player player = Bukkit.getPlayer(u);
+            if (player != null) {
+                Location loc = player.getLocation().clone();
 
-				int ran1 = getRandomNumber();
-				int ran2 = getRandomNumber();
+                loc = getSafeLoc(world, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 
-				x = x + ran1;
-				z = z + ran2;
+                if (loc != null && game.getGameArenaData().isInRegion(loc)) {
+                    MobEntry mobEntry;
+                    if (isDay(world)) {
+                        mobEntry = dayMobs.get(random.nextInt(dayMobs.size()));
+                    } else {
+                        mobEntry = nightMobs.get(random.nextInt(nightMobs.size()));
+                    }
+                    mobEntry.spawn(loc);
+                }
+            }
+        }
+    }
 
-				location = getSafeLoc(world, x, y, z);
+    private boolean isDay(World w) {
+        long time = w.getTime();
+        return time < 12542 || time > 23460;
+    }
 
-				if (location != null && game.getGameArenaData().isInRegion(location)) {
-					MobEntry mobEntry;
-					assert world != null;
-					if (isDay(world)) {
-						mobEntry = mobManager.getDayMobs().get(rg.nextInt(mobManager.getDayMobs().size()));
-					} else {
-						mobEntry = mobManager.getNightMobs().get(rg.nextInt(mobManager.getNightMobs().size()));
-					}
-					mobEntry.spawn(location);
-					//w.spawnEntity(l, pickRandomMob(!isDay(w), rg.nextInt(10)));
-				}
-			}
-		}
-	}
+    private int getRandomNumber() {
+        int r = random.nextInt(20) + 6;
+        return random.nextBoolean() ? r : -r;
 
-	private boolean isDay(World w) {
-		long time = w.getTime();
-		return time < 12300 || time > 23850;
-	}
-	/*  NEW MOB SPAWNING ENGINE IN PLAY - leaving this here for a while, just in case
-	private EntityType pickRandomMob(boolean isNight, int x) {
-		if (isNight) {
-			if (x < 3)
-				return EntityType.ZOMBIE;
-			if (x < 5)
-				return EntityType.DROWNED;
-			if (x < 7)
-				return EntityType.SKELETON;
-			if (x < 8)
-				return EntityType.STRAY;
+    }
 
-			return EntityType.CREEPER;
-		} else {
-			if (x < 2)
-				return EntityType.COW;
-			if (x < 5)
-				return EntityType.PIG;
-			if (x < 6)
-				return EntityType.CHICKEN;
-			if (x < 7)
-				return EntityType.SHEEP;
-			if (x < 8)
-				return EntityType.PARROT;
-			if (x < 9)
-				return EntityType.SPIDER;
+    private Location getSafeLoc(World w, int x, int y, int z) {
+        int trys = 30;
 
-			return EntityType.CREEPER;
-		}
-	}
+        x = x + getRandomNumber();
+        z = z + getRandomNumber();
 
-	 */
+        while (trys > 0) {
+            trys--;
 
-	private int getRandomNumber() {
-		int r = rg.nextInt(25) - rg.nextInt(25);
-		if (r <= 6 && r >= -6) {
-			return getRandomNumber();
-		}
-		return r;
-	}
+            Material material = w.getBlockAt(x, y, z).getType();
+            Material below = w.getBlockAt(x, y - 1, z).getType();
+            Material above = w.getBlockAt(x, y + 1, z).getType();
 
-	private Location getSafeLoc(World w, int x, int y, int z) {
-		int trys = 30;
+            if (material.isSolid()) {
+                y++;
+            } else if (below == Material.AIR) {
+                y--;
+            } else if (below == Material.WATER || below == Material.LAVA || above.isSolid()) {
+                x = x + getRandomNumber();
+                z = z + getRandomNumber();
+            } else {
+                return new Location(w, x, y, z);
+            }
+        }
+        return null;
+    }
 
-		while (trys > 0) {
-
-			trys--;
-
-			Material m = w.getBlockAt(x, y, z).getType();
-
-			if (m.isSolid()) {
-				y++;
-			} else if (w.getBlockAt(x, y - 1, z).getType() == Material.AIR) {
-				y--;
-			} else if (w.getBlockAt(x, y + 1, z).getType().isSolid()) {
-				int ran1 = getRandomNumber();
-				int ran2 = getRandomNumber();
-
-				x = x + ran1;
-				z = z + ran2;
-			} else {
-				return new Location(w, x, y, z);
-			}
-		}
-		return null;
-	}
-
-	public void stop() {
-		Bukkit.getScheduler().cancelTask(id);
-	}
+    public void stop() {
+        Bukkit.getScheduler().cancelTask(id);
+    }
 
 }
