486c0edfc0e58f89be969437cbb2ef79888894cf
Shane Bee <shanebolenback@me.com>
2020-10-28 04:44:11 -0700

    PlayerData - add getter for bukkit player - Code cleanup as well

--- src/main/java/tk/shanebee/hg/data/PlayerData.java
+++ src/main/java/tk/shanebee/hg/data/PlayerData.java
@@ -20,130 +20,159 @@ import java.util.UUID;
 @SuppressWarnings("WeakerAccess")
 public class PlayerData implements Cloneable {
 
-	//Pregame data
-	private final ItemStack[] inv;
-	private final ItemStack[] equip;
-	private final int expL;
-	private final float expP;
-	private final double health;
-	private final int food;
-	private final float saturation;
-	private final GameMode mode;
-	private final UUID uuid;
-	private final Scoreboard scoreboard;
-	
-	//InGame data
-	private Team team;
-	private Team pendingTeam;
-	private final Game game;
+    //Pregame data
+    private final ItemStack[] inv;
+    private final ItemStack[] equip;
+    private final int expL;
+    private final float expP;
+    private final double health;
+    private final int food;
+    private final float saturation;
+    private final GameMode mode;
+    private final UUID uuid;
+    private final Scoreboard scoreboard;
 
-	/** New player pre-game data file
-	 * @param player Player to save
-	 * @param game Game they will be entering
-	 */
-	public PlayerData(Player player, Game game) {
-		this.game = game;
-		this.uuid = player.getUniqueId();
-		inv = player.getInventory().getContents();
-		equip = player.getInventory().getArmorContents();
-		expL = player.getLevel();
-		expP = player.getExp();
-		mode = player.getGameMode();
-		food = player.getFoodLevel();
-		saturation = player.getSaturation();
-		health = player.getHealth();
-		Util.clearInv(player);
-		player.setLevel(0);
-		player.setExp(0);
-		scoreboard = player.getScoreboard();
-	}
+    //InGame data
+    private Team team;
+    private Team pendingTeam;
+    private final Game game;
 
-	/** Restore a player's saved data
-	 * @param player Player to restore data to
-	 */
-	public void restore(Player player) {
-		if (player == null) return;
-		Util.clearInv(player);
-		player.setWalkSpeed(0.2f);
-		player.setLevel(expL);
-		player.setExp(expP);
-		player.setFoodLevel(food);
-		player.setSaturation(saturation);
-		player.getInventory().setContents(inv);
-		player.getInventory().setArmorContents(equip);
-		player.setGameMode(mode);
-		player.updateInventory();
-		player.setInvulnerable(false);
-		restoreHealth(player);
-		player.setScoreboard(scoreboard);
-	}
+    /**
+     * New player pre-game data file
+     *
+     * @param player Player to save
+     * @param game   Game they will be entering
+     */
+    public PlayerData(Player player, Game game) {
+        this.game = game;
+        this.uuid = player.getUniqueId();
+        inv = player.getInventory().getContents();
+        equip = player.getInventory().getArmorContents();
+        expL = player.getLevel();
+        expP = player.getExp();
+        mode = player.getGameMode();
+        food = player.getFoodLevel();
+        saturation = player.getSaturation();
+        health = player.getHealth();
+        Util.clearInv(player);
+        player.setLevel(0);
+        player.setExp(0);
+        scoreboard = player.getScoreboard();
+    }
 
-	// Restores later if player has an item in their inventory which changes their max health value
-	@SuppressWarnings("ConstantConditions")
+    /**
+     * Restore a player's saved data
+     *
+     * @param player Player to restore data to
+     */
+    public void restore(Player player) {
+        if (player == null) return;
+        Util.clearInv(player);
+        player.setWalkSpeed(0.2f);
+        player.setLevel(expL);
+        player.setExp(expP);
+        player.setFoodLevel(food);
+        player.setSaturation(saturation);
+        player.getInventory().setContents(inv);
+        player.getInventory().setArmorContents(equip);
+        player.setGameMode(mode);
+        player.updateInventory();
+        player.setInvulnerable(false);
+        restoreHealth(player);
+        player.setScoreboard(scoreboard);
+    }
+
+    // Restores later if player has an item in their inventory which changes their max health value
+    @SuppressWarnings("ConstantConditions")
     private void restoreHealth(Player player) {
-		double att = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
-		if (health > att) {
-			Bukkit.getScheduler().runTaskLater(HG.getPlugin(), () -> player.setHealth(health), 10);
-		} else {
-			player.setHealth(health);
-		}
-	}
+        double att = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        if (health > att) {
+            Bukkit.getScheduler().runTaskLater(HG.getPlugin(), () -> player.setHealth(health), 10);
+        } else {
+            player.setHealth(health);
+        }
+    }
 
-	/** Check if a player is on a team
-	 * @param uuid Uuid of player to check
-	 * @return True if player is on a team
-	 */
-	public boolean isOnTeam(UUID uuid) {
-		return (team != null && team.isOnTeam(uuid));
-	}
+    /**
+     * Check if a player is on a team
+     *
+     * @param uuid Uuid of player to check
+     * @return True if player is on a team
+     */
+    public boolean isOnTeam(UUID uuid) {
+        return (team != null && team.isOnTeam(uuid));
+    }
 
-	/** Get the game of this player data
-	 * @return The game of this player data
-	 */
-	public Game getGame() {
-		return game;
-	}
+    /**
+     * Get the game of this player data
+     *
+     * @return The game of this player data
+     */
+    public Game getGame() {
+        return game;
+    }
 
-	/** Get the team of this player data
-	 * @return The team
-	 */
-	public Team getTeam() {
-		return team;
-	}
+    /**
+     * Get the team of this player data
+     *
+     * @return The team
+     */
+    public Team getTeam() {
+        return team;
+    }
 
-	/** Set the team of this player data
-	 * @param team The team to set
-	 */
-	public void setTeam(Team team) {
-		this.team = team;
-	}
+    /**
+     * Set the team of this player data
+     *
+     * @param team The team to set
+     */
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
-    /** Get the pending team of this player data
+    /**
+     * Get the pending team of this player data
+     *
      * @return Pending team of this player data
      */
     public Team getPendingTeam() {
         return pendingTeam;
     }
 
-    /** Set the pending team of this player data
+    /**
+     * Set the pending team of this player data
+     *
      * @param pendingTeam Team for pending
      */
     public void setPendingTeam(Team pendingTeam) {
         this.pendingTeam = pendingTeam;
     }
 
-    /** Get the gamemode of this player data
-	 * @return Gamemode of this player data
-	 */
-	public GameMode getGameMode() {
-		return this.mode;
-	}
+    /**
+     * Get the gamemode of this player data
+     *
+     * @return Gamemode of this player data
+     */
+    public GameMode getGameMode() {
+        return this.mode;
+    }
 
-    /** Get the UUID belonging to this player data
+    /**
+     * Get the UUID belonging to this player data
+     *
      * @return UUID belonging to this player data
      */
-	public UUID getUuid() {
-	    return this.uuid;
+    public UUID getUuid() {
+        return this.uuid;
+    }
+
+    /**
+     * Get the {@link Player Bukkit Player} belonging to this player data
+     *
+     * @return Player belonging to this player data
+     */
+    public Player getBukkitPlayer() {
+        return Bukkit.getPlayer(this.uuid);
     }
 
     @Override
