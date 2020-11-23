77a8f44567a485e72961cc12f2908ff03fb90dea
ShaneBee <shanebolenback@me.com>
2020-11-23 14:35:30 -0800

    PlayerData - update javadoc for new previous location methods

--- src/main/java/tk/shanebee/hg/data/PlayerData.java
+++ src/main/java/tk/shanebee/hg/data/PlayerData.java
@@ -169,10 +169,20 @@ public class PlayerData implements Cloneable {
         return this.uuid;
     }
 
+    /**
+     * Set the previous location of the player
+     *
+     * @param previousLocation Location player was at before entering arena
+     */
     public void setPreviousLocation(Location previousLocation) {
         this.previousLocation = previousLocation;
     }
 
+    /**
+     * Get the previous location of the player
+     *
+     * @return Location player was at before entering arena
+     */
     @Nullable
     public Location getPreviousLocation() {
         if (previousLocation != null) {
