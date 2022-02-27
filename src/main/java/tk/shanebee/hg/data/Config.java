c0361d8daf528e4527c2349fb264963da2e4a1f6
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-02-27 17:49:23 -0500

    Update src/main/java/tk/shanebee/hg/data/Config.java
    Co-authored-by: Shane Bee <shanebolenback@me.com>

--- src/main/java/tk/shanebee/hg/data/Config.java
+++ src/main/java/tk/shanebee/hg/data/Config.java
@@ -1,6 +1,5 @@
 package tk.shanebee.hg.data;
 
-import org.bukkit.Material;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
