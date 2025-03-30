64ffbd9e1599bd6b3a139a9e182d005733fdcc74
ShaneBeee <shanebolenback@me.com>
2025-03-30 15:39:13 -0700

    CustomArg - add getter for game that will auto throw error

--- src/main/java/com/shanebeestudios/hg/api/command/CustomArg.java
+++ src/main/java/com/shanebeestudios/hg/api/command/CustomArg.java
@@ -1,11 +1,15 @@
 package com.shanebeestudios.hg.api.command;
 
 import com.shanebeestudios.hg.HungerGames;
+import com.shanebeestudios.hg.game.Game;
 import com.shanebeestudios.hg.managers.GameManager;
+import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.CustomArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
+import dev.jorel.commandapi.exceptions.WrapperCommandSyntaxException;
+import dev.jorel.commandapi.executors.ExecutionInfo;
 
 import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
@@ -13,6 +17,15 @@ import java.util.concurrent.CompletableFuture;
 public abstract class CustomArg {
 
     private static final GameManager GAME_MANAGER = HungerGames.getPlugin().getGameManager();
+
+    public static Game getGame(ExecutionInfo<?,?> info) throws WrapperCommandSyntaxException {
+        Game game = info.args().getByClass("game", Game.class);
+        if (game == null) {
+            String raw = info.args().getRaw("game");
+            throw CommandAPI.failWithString("invalid game '" + raw + "'");
+        }
+        return game;
+    }
 
     public static final CustomArg GAME = new CustomArg() {
         @Override
