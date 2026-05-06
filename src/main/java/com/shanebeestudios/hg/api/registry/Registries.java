a8896720e9c99eb5f42ebf798414b45a31e15ac0
ShaneBeee <shanebolenback@me.com>
2026-05-05 21:44:05 -0700

    Registries - cleanup

--- src/main/java/com/shanebeestudios/hg/api/registry/Registries.java
+++ src/main/java/com/shanebeestudios/hg/api/registry/Registries.java
@@ -19,14 +19,15 @@ import org.bukkit.potion.PotionType;
 @SuppressWarnings({"NullableProblems", "UnstableApiUsage"})
 public class Registries {
 
-    public static final Registry<Attribute> ATTRIBUTE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.ATTRIBUTE);
-    public static final Registry<BlockType> BLOCK_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.BLOCK);
-    public static final Registry<DamageType> DAMAGE_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.DAMAGE_TYPE);
-    public static final Registry<DataComponentType> DATA_COMPONENT_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.DATA_COMPONENT_TYPE);
-    public static final Registry<Enchantment> ENCHANTMENT_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT);
-    public static final Registry<EntityType> ENTITY_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENTITY_TYPE);
-    public static final Registry<ItemType> ITEM_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.ITEM);
-    public static final Registry<PotionEffectType> POTION_EFFECT_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.MOB_EFFECT);
-    public static final Registry<PotionType> POTION_TYPE_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.POTION);
+    private static final RegistryAccess ACCESS = RegistryAccess.registryAccess();
+    public static final Registry<Attribute> ATTRIBUTE_REGISTRY = ACCESS.getRegistry(RegistryKey.ATTRIBUTE);
+    public static final Registry<BlockType> BLOCK_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.BLOCK);
+    public static final Registry<DamageType> DAMAGE_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.DAMAGE_TYPE);
+    public static final Registry<DataComponentType> DATA_COMPONENT_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.DATA_COMPONENT_TYPE);
+    public static final Registry<Enchantment> ENCHANTMENT_REGISTRY = ACCESS.getRegistry(RegistryKey.ENCHANTMENT);
+    public static final Registry<EntityType> ENTITY_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.ENTITY_TYPE);
+    public static final Registry<ItemType> ITEM_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.ITEM);
+    public static final Registry<PotionEffectType> POTION_EFFECT_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.MOB_EFFECT);
+    public static final Registry<PotionType> POTION_TYPE_REGISTRY = ACCESS.getRegistry(RegistryKey.POTION);
 
 }
