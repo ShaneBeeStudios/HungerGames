b532758c05268373dc1006543b1c6bc8f62ce8e0
ShaneBeee <shanebolenback@me.com>
2026-05-14 20:39:26 -0700

    Data - add more data

--- src/main/java/com/shanebeestudios/hg/api/game/Data.java
+++ src/main/java/com/shanebeestudios/hg/api/game/Data.java
@@ -2,6 +2,7 @@ package com.shanebeestudios.hg.api.game;
 
 import com.shanebeestudios.hg.plugin.HungerGames;
 import com.shanebeestudios.hg.plugin.configs.Language;
+import com.shanebeestudios.hg.plugin.managers.GameManager;
 
 /**
  * General class for storing different aspects of data for {@link Game Games}
@@ -10,30 +11,50 @@ public abstract class Data {
 
     final Game game;
     final HungerGames plugin;
+    final GameManager gameManager;
     final Language lang;
 
     Data(Game game) {
         this.game = game;
         this.plugin = game.plugin;
+        this.gameManager = this.plugin.getGameManager();
         this.lang = game.lang;
     }
 
     /**
-     * Get the {@link Game} this data belongs to
+     * Get the {@link Game} this data belongs to.
      *
      * @return Game this data belongs to
      */
     public Game getGame() {
-        return game;
+        return this.game;
     }
 
     /**
-     * Quick method to access the main plugin
+     * Quick method to access the main plugin.
      *
      * @return Instance of {@link HungerGames plugin}
      */
     public HungerGames getPlugin() {
-        return plugin;
+        return this.plugin;
+    }
+
+    /**
+     * Get the {@link GameManager} for the plugin.
+     *
+     * @return Instance of {@link GameManager}
+     */
+    public GameManager getGameManager() {
+        return this.gameManager;
+    }
+
+    /**
+     * Get the {@link Language} for the plugin.
+     *
+     * @return Instance of {@link Language}
+     */
+    public Language getLang() {
+        return this.lang;
     }
 
 }
