d1495de29b4d46449a38c3c35a31235ed61c1897
ShaneBeee <shanebolenback@me.com>
2026-05-20 15:24:47 -0700

    Util - version check updates

--- src/main/java/com/shanebeestudios/hg/api/util/Util.java
+++ src/main/java/com/shanebeestudios/hg/api/util/Util.java
@@ -215,26 +215,26 @@ public class Util {
     }
 
     /**
-     * Check if server is running a minimum Minecraft version
+     * Check if the server is running a minimum Minecraft version.
      *
-     * @param major Major version to check (Most likely just going to be 1)
-     * @param minor Minor version to check
-     * @return True if running this version or higher
+     * @param year Year version to check
+     * @param drop Drop version to check
+     * @return True if running this version or higher, otherwise false
      */
-    public static boolean isRunningMinecraft(int major, int minor) {
-        return isRunningMinecraft(major, minor, 0);
+    public static boolean isRunningMinecraft(int year, int drop) {
+        return isRunningMinecraft(year, drop, 0);
     }
 
     /**
-     * Check if server is running a minimum Minecraft version
+     * Check if the server is running a minimum Minecraft version.
      *
-     * @param major    Major version to check (Most likely just going to be 1)
-     * @param minor    Minor version to check
+     * @param year     Year version to check
+     * @param drop     Drop version to check
      * @param revision Revision to check
-     * @return True if running this version or higher
+     * @return True if running this version or higher, otherwise false
      */
-    public static boolean isRunningMinecraft(int major, int minor, int revision) {
-        String[] version = Bukkit.getServer().getBukkitVersion().split("-")[0].split("\\.");
+    public static boolean isRunningMinecraft(int year, int drop, int revision) {
+        String[] version = Bukkit.getServer().getMinecraftVersion().split("\\.");
         int maj = Integer.parseInt(version[0]);
         int min = Integer.parseInt(version[1]);
         int rev;
@@ -243,7 +243,13 @@ public class Util {
         } catch (Exception ignore) {
             rev = 0;
         }
-        return maj > major || min > minor || (min == minor && rev >= revision);
+        if (maj > year) {
+            return true;
+        } else if (maj == year && min > drop) {
+            return true;
+        } else {
+            return maj == year && min == drop && rev >= revision;
+        }
     }
 
     /**
