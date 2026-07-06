f57fcb9535c41ad333e363349afe765bbc7a34fe
ShaneBee <shanebolenback@me.com>
2026-07-06 08:19:31 -0700

    build.gradle.kts - update MC/Paper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ val projectVersion = "5.0.0"
 // Minimum version of Minecraft that HungerGames supports
 val apiVersion = "1.21.10"
 // Where this builds on the server
-val serverLocation = "26-1"
+val serverLocation = "26-2"
 // Minecraft version to build against
-val minecraftVersion = "26.1.2"
+val minecraftVersion = "26.2"
 
 java.sourceCompatibility = JavaVersion.VERSION_25
 
